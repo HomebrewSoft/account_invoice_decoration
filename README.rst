a4bb3209f0481e7931bf6f0165b4ae142e7faea0
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-12-15 11:58:00 -0600

    [INIT]

---
+++ README.rst
@@ -0,0 +1,4 @@
+Partner Unique VAT
+==================
+
+Shows an alert if the VAT or the name of the partner is al ready registered.
---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Account Invoice Decoration',
+    'version': '1.0.0',
+    'author': 'HomebrewSoft',
+    'website': 'https://github.com/HomebrewSoft/account_invoice_decoration',
+    'category': 'Operations',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/account_invoice.xml',
+    ],
+}
